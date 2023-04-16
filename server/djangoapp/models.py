5b32149e5053bbca17dca1dfb75aeadf644a7ca7
Luis Alvarez <la.alv.canab@gmail.com>
2023-04-16 03:04:20 -0400

    adding CarMake and CarModels

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -19,9 +19,73 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+# Car Make model
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=50)
+    description = models.CharField(null=True, max_length=500)
 
+    def __str__(self):
+        return self.name
+
+# Car Model model
+class CarModel(models.Model):
+    id = models.IntegerField(default=1,primary_key=True)
+    name = models.CharField(null=False, max_length=100, default='Car')
+   
+    SEDAN = 'Sedan'
+    SUV = 'SUV'
+    WAGON = 'Wagon'
+    MINIVAN = 'Minivan'
+    CAR_TYPES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon'),
+        (MINIVAN, 'Minivan')
+    ]
+
+    type = models.CharField(
+        null=False,
+        max_length=50,
+        choices=CAR_TYPES,
+        default=SEDAN
+    )
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    year = models.DateField(default=now)
+
+    def __str__(self):
+        return "Name: " + self.name
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
+class CarDealer:
+    def __init__(self, address, city, full_name, id, 
+                    lat, long, short_name, st, zip):
+        self.address = address
+        self.city = city
+        self.full_name = full_name
+        self.id = id
+        self.lat = lat
+        self.long = long
+        self.short_name = short_name
+        self.st = st
+        self.zip = zip
+
+    def __str__(self):
+        return self.full_name
 
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+class DealerReview:
+    def __init__(self, dealership, name, purchase, review):
+        self.make = ""
+        self.car_model = ""
+        self.car_year = ""
+        self.dealership = dealership
+        self.id = ""  # The id of the review
+        self.name = name  # Name of the reviewer
+        self.purchase = purchase  # Did the reviewer purchase the car? bool
+        self.purchase_date = ""
+        self.review = review  # The actual review text
+        self.sentiment = ""  # Watson NLU sentiment analysis of review
+
+    def __str__(self):
+        return "Reviewer: " + self.name + " Review: " + self.review
