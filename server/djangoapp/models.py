fb1ba9aadcaae275731a63da44e3dbaf41e87737
Luis Alvarez <la.alv.canab@gmail.com>
2023-04-16 04:49:21 -0400

    adding new features

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -70,7 +70,7 @@ class CarDealer:
         self.zip = zip
 
     def __str__(self):
-        return self.full_name
+        return "Dealer name: " + self.full_name
 
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -29,7 +29,9 @@ urlpatterns = [
     path(route="about/", view=views.about, name="about"),
 
     # path for dealer reviews view
+    path(route='dealer/<int:id>/', view=views.get_dealer_details, name='dealer_details'),
 
     # path for add a review view
+    path(route='dealer/<int:id>/add-review/', view=views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
